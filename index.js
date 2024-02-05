9fa9309c85545989fb714349a6e6232e7d4ebf6c
GabrielKuiawa <gabrielsantos77239@gmail.com>
2024-02-05 03:03:41 -0300

    Otimizando o código

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
 
+let url_authentication = "https://instance.fique.online/webhook/merge/88d8701e-a1d6-4fee-b15b-53e90dc1d126/autenticacao/57441afd5a59ccd4c62816683fcc8d665c42bb7b12857fc64a6cace4ababdc67f78c70b044";
+let url_list_pilares = "https://instance.fique.online/webhook/merge/88d8701e-a1d6-4fee-b15b-53e90dc1d126/listar_pilares/76b07f1dbf18eabde7b8e3611ab078daa0f34b094cc9856d20d6d0b15fb3b7a99f697e451d";
+let url_send_response = "https://instance.fique.online/webhook/merge/88d8701e-a1d6-4fee-b15b-53e90dc1d126/envia_resposta/7b56940678e89802e02e1981a8657206d639f657d4c58efb8d8fb74814799d1c001ec121c6";
+
 const auth = async () => {
-  const session_url = "https://instance.fique.online/webhook/merge/88d8701e-a1d6-4fee-b15b-53e90dc1d126/autenticacao/57441afd5a59ccd4c62816683fcc8d665c42bb7b12857fc64a6cace4ababdc67f78c70b044";
+  const session_url = url_authentication;
   const response = await axios.post(session_url, {}, {
       auth: {
         username: "teste_fiqon",
@@ -13,31 +17,33 @@ const auth = async () => {
 }
 
 const pillarsFiqOn = async () => {
-  const session_url = "https://instance.fique.online/webhook/merge/88d8701e-a1d6-4fee-b15b-53e90dc1d126/listar_pilares/76b07f1dbf18eabde7b8e3611ab078daa0f34b094cc9856d20d6d0b15fb3b7a99f697e451d";
+  const session_url = url_list_pilares;
   
   let api_token = await auth();
   let page = 0;
+  let more_items = true;
   let pillars = "";
 
-
-  while(page < 5) {
+  while(more_items) {
     let url_page = session_url + `?page=${page}&api_token=${api_token}`
     const response = await axios.get(url_page);
+
     page = response.data.next_page;
+    more_items = (response.data.more_items == "true");
     pillars += response.data.data;
+
     console.log(`${page}º ${response.data.data}`);
   }  
 
-
-  return pillars;
+  return {data : pillars, token : api_token};
 }
 
 const sendPillars = async () => {
-  const session_url = "https://instance.fique.online/webhook/merge/88d8701e-a1d6-4fee-b15b-53e90dc1d126/envia_resposta/7b56940678e89802e02e1981a8657206d639f657d4c58efb8d8fb74814799d1c001ec121c6?api_token=yObzWFzqFmXe7tL2hA2NZvUsUb6hoQz79FrDueFq";
-  let base64 = Buffer.from(await pillarsFiqOn()).toString('base64');
+  pillars = await pillarsFiqOn();
+  const session_url = url_send_response + `?api_token=${pillars.token}`;
+  let base64 = Buffer.from(pillars.data).toString('base64');
   const body = {"answer" : base64 };
   const response = await axios.post(session_url,body);
-  
   
   console.log(response.data);
 }
